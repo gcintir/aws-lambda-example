4436ee1bf54d65ae206128a0f7c118137d5c19fd
DIR\guray.cintir <guray.cintir@accenture.com>
2022-06-07 23:15:31 +0300

    some functions

--- lambda/index.js
+++ lambda/index.js
@@ -1,15 +1,34 @@
-const lambda = async (event) => {
-    console.log(`Received event ${JSON.stringify(event)}`);
+const lambda = async (event, context) => {
     try {
+        // logging info about received event, context and environment variables
+        logReceivedEvent(event);
+        logCurrentContext(context);
+        logEnvironmentVariables();
+
         const min = 1;
         const max = 100;
         const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
         const response = `Generated number ${randomNumber}`;
 
+        console.log('Remaining time: ', context.getRemainingTimeInMillis());
         return response;
     } catch (error) {
         console.error(error);
     }
 }
+
+const logReceivedEvent = (receivedEvent) => {
+    console.log(`Received event ${JSON.stringify(receivedEvent)}`);
+};
+
+const logCurrentContext = (currentContext) => {
+    console.log(`Current context ${JSON.stringify(currentContext)}`);
+}
+
+const logEnvironmentVariables = () => {
+    console.log(`Current environment variables ${JSON.stringify(process.env)}`);
+}
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
 exports.lambda = lambda;
